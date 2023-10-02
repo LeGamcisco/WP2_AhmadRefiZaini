14c5d941569ce918e3fadc654a02aaab054e1b6e
LeGamcisco <ahmdrfzn505@gmail.com>
2023-10-02 07:55:02 +0700

    Latihan Pertemuan 2

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php 
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Ahmad Refi Zaini
+              Saya tinggal di daerah Bojong Gede
+              Olah Raga Kesukaan saya adalah Basket";
+    }
+}
+
+?>
