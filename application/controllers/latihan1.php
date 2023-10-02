a2a92ec6a164aef83bd591c3b734471a2c60b516
LeGamcisco <ahmdrfzn505@gmail.com>
2023-10-02 08:26:03 +0700

    Latihan Pertemuan 3

--- application/controllers/latihan1.php
+++ application/controllers/latihan1.php
@@ -1,17 +1,21 @@
-<?php 
-class latihan1 extends CI_Controller
+<?php
+class Latihan1 extends CI_Controller
 {
     public function index()
     {
-        echo "Selamat Datang... Selamat Belajar Web Programming";
+        echo "Selamat Datang.. Selamat Belajar Web Programming";
     }
-
-
+    
     public function penjumlahan($n1, $n2)
     {
-        $this -> load -> model('model_latihan1.php');
-        $hasil = $this -> model_latihan1 -> jumlah($n1, $n2);
-        echo "Hasil penjumlahan dari". $n1 ." + ". $n2 ." = ". $hasil;
+        $this->load->model('model_latihan1');
+        // $hasil = $this->model_latihan1->jumlah($n1, $n2);
+
+        $data['nilai1'] = $n1;
+        $data['nilai2'] = $n2;
+        $data['hasil'] = $this->model_latihan1->jumlah($n1, $n2);
+        
+         $this->load->view('view-latihan', $data);
     }
 }
 ?>
