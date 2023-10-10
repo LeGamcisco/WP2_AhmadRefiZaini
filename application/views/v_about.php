d9b2bb40c35a7573e968d4f84da7aee17b4f4787
LeGamcisco <ahmdrfzn505@gmail.com>
2023-10-10 20:02:43 +0700

    Tugas Pertemuan 4

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -1,16 +1,34 @@
 <section>
     <h1><?php echo $judul ?></h1>
+    <h4>NIM</h4>
+    <ul type="none">
+        <li>19220057</li>
+    </ul>
     <h4>Nama</h4>
     <ul type="disc">
         <li>Nama Depan : Ahmad</li>
         <li>Nama Belakang : Refi Zaini</li>
     </ul>
-    <br>
+    <h4>Kelas</h4>
+    <ul type="none">
+        <li>19.3D.01</li>
+    </ul>
+    <h4>Program Studi</h4>
+    <ul type="none">
+        <li>Sistem Informasi</li>
+    </ul>
+    <h4>Nomor Telepon</h4>
+    <ul type="none">
+        <li>0813-8587-6527</li>
+    </ul>
+    <h4>Email</h4>
+    <ul type="none">
+        <li>ahmadrefi05@gmail.com</li>
+    </ul>
     <h4>Alamat</h4>
     <ul type="none">
         <li> Gg. Langgar, Kec. Bojong Gede, Kabupaten Bogor</li>
     </ul>
-
     <h4>Tempat Lahir</h4>
     <ul type="none">
         <li>Depok</li>
